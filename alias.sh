98f6632eb33905e980cdb1c2145ef64f7d8c40b6
Muhammad Nawfal Meeran <90894918+mnawfal29@users.noreply.github.com>
2022-05-30 17:05:48 +0400

    Create alias.sh

---
+++ alias.sh
@@ -0,0 +1,5 @@
+echo "alias genUser='./genUser.sh'
+alias permit='./permit.sh'
+alias updateBranch='./updateBranch.sh'
+alias allotInterest='./allotInterest.sh'
+alias makeTransaction='./makeTransaction.sh'"
