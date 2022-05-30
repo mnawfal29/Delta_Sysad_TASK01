11bc82341a6650683aec1ea27b4fbd7b7f187e46
Muhammad Nawfal Meeran <90894918+mnawfal29@users.noreply.github.com>
2022-05-30 17:33:32 +0400

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -2,4 +2,10 @@ echo "alias genUser='./genUser.sh'
 alias permit='./permit.sh'
 alias updateBranch='./updateBranch.sh'
 alias allotInterest='./allotInterest.sh'
-alias makeTransaction='./makeTransaction.sh'"
+alias makeTransaction='./makeTransaction.sh'" >> /home/user/.bashrc
+
+for i in {1..4}
+do
+echo "alias updateBranch='./updateBranch.sh'
+alias makeTransaction='./makeTransaction.sh'" >> /home/bank/MGR/MGR_Branch$i/.bashrc
+done
